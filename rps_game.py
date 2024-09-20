6c11f3f94a890f839964801c2745602019efed40
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-09-20 22:27:27 +0300

    Refactor: Simplify logic with separate focused functions.

--- rps_game.py
+++ rps_game.py
@@ -2,35 +2,40 @@ import random
 import sys
 
 
-def rps_game():
+def get_user_choice():
+    """Handle user input and ensure it's valid."""
     while True:
         try:
             user = int(input("Please, enter:\n1 for Rock\n2 for Paper\n3 for Scissors\n"))
             
             # Validate user input.
             if user in [1, 2, 3]:
-                break
+                return user             # <== NOTE that we return the user value
             raise ValueError
-            
-        except ValueError:
-            print("\nInvalid input. You must enter 1, 2, or 3.")
         
+        except ValueError:
+            print("\nInvalid input. You must enter 1, 2, or 3.\n")
+
+def decide_winner(player, opponent):
+    """Determine the result of the game."""
+    if player == 1 and opponent == 3:
+        return "🎉🎉 You won!"
+    elif player == 2 and opponent == 1:
+        return "🎉🎉 You won!"
+    elif player == 3 and opponent == 2:
+        return "🎉🎉 You won!"
+    elif player == opponent:
+        return "🤝 Tie Game!"
+    else:
+        return "😥 You lost .. better luck next time."
+
+
+def rps_game():
+    user = get_user_choice()
     computer = random.choice([1, 2, 3])
     
     print(f"\nYou chose: {user}")
     print(f"Computer chose: {computer}\n")
-    
-    def decide_winner(player, opponent):
-        if player == 1 and opponent == 3:
-            return "🎉🎉 You won!"
-        elif player == 2 and opponent == 1:
-            return "🎉🎉 You won!"
-        elif player == 3 and opponent == 2:
-            return "🎉🎉 You won!"
-        elif player == opponent:
-            return "🤝 Tie Game!"
-        else:
-            return "😥 You lost .. better luck next time."
     
     result = decide_winner(user, computer)
     print(result)
